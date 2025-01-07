2fb2741f4c8d167cb88763bd089f2c98eacaf30d
Leeiazz <leeiaz7554@gmail.com>
2025-01-07 08:28:00 -0300

    Folders and app created

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { JournalApp } from './JournalApp';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola</h1>
+    <JournalApp />
   </StrictMode>
 );
